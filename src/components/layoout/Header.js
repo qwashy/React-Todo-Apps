8cf5d2ad50487bc4ba169efc7975a3dcf130f875
rizkyjpr <qwashy39@mail.ugm.ac.id>
2020-12-07 23:21:12 +0700

    commit

--- src/components/layoout/Header.js
+++ src/components/layoout/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Header() {
     return (
         <header style={headerStyle}>
-            <h1>TodoList</h1>
+            <h1>Kontrakan List</h1>
             <Link to='/' style={linkStyle}>Home</Link> | <Link to='/about' style={linkStyle}>About</Link>
         </header>
     )
